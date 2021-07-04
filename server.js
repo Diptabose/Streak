9382d0b695beb8dad8953fb92a1f75871dd66d16
Diptabose <83805205+Diptabose@users.noreply.github.com>
2021-07-04 18:22:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express=require('express');
 const socketIO=require('socket.io')
 const PORT=process.env.PORT||3000;
 const INDEX='/Chat.html';
-const server=express().use((req,res)=>res.sendFile(INDEX,{root:__dirname})).listen(PORT,()=>console.log('listening on some port'));
+const server=express();
+server.use(express.static("public"));
+server.use((req,res)=>{
+  res.sendFile(INDEX,{root:public})
+  });
+server.listen(PORT,()=>{
+  console.log('listening on some port')}
+        );
 const io=socketIO(server);
 const users={};
 io.on('connection',socket=>{
