6a9af5e651a969acb1b61f9adf118649a2fe57ed
Diptabose <83805205+Diptabose@users.noreply.github.com>
2021-07-04 17:56:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express=require('express');
+const socketIO=require('socket.io')
+const PORT=process.env.PORT||3000;
+const INDEX='/Chat.html';
+const server=express().use((req,res)=>res.sendFile(INDEX,{root:__dirname})).listen(PORT,()=>console.log('listening on some port'));
+const io=socketIO(server);
+const users={};
+io.on('connection',socket=>{
+  socket.on('new-user-joined',name=>{
+    console.log('User',name);
+    users[socket.id]=name;
+    socket.broadcast.emit('user-joined',name);
+  });
+
+  socket.on('send',message=>{
+    socket.broadcast.emit('recieve',{message:message,name:users[socket.id]});
+  });
+
+  socket.on('disconnect',message=>{
+    socket.broadcast.emit('left',users[socket.id]);
+    delete users[socket.id];
+  });
+
+});
